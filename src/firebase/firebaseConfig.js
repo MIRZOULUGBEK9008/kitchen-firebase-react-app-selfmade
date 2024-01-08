a7da51ea92ca2e4227f1dca11fa200ccae7d0c94
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-01-08 20:08:37 +0500

    Initial commit

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5yyTU1IZsxxju2AJ1r9otr9Pfc1NtEi4",
+  authDomain: "kitchen-firebase-react-app.firebaseapp.com",
+  projectId: "kitchen-firebase-react-app",
+  storageBucket: "kitchen-firebase-react-app.appspot.com",
+  messagingSenderId: "416565997062",
+  appId: "1:416565997062:web:8a0e99796e9747338f9d87",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="hover:text-yellow-400">Developing...</h1>;
+}
+
+export default App;
